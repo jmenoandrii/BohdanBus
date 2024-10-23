9ee11ff87f2f0e34357356b9f3cc420737e36e31
Andrii <andrii.dryhaniuk@gmail.com>
2024-10-23 15:41:01 +0300

    Hard Work #3
    -Updated bus model
    -Completed terrain textures
    -Fixed wipers, mirrors
    -Fixed many bugs

--- BogdanBus/Assets/Scripts/WiperButton.cs
+++ BogdanBus/Assets/Scripts/WiperButton.cs
@@ -2,13 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 public class WiperButton : MonoBehaviour, IInteractableObject
 {
     [SerializeField]
     private Wipers _wiper;
 
+    private bool _active = false;
+    private Animator _animator;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
     public void Interact()
     {
         _wiper.Interact();
+        _active = !_active;
+        _animator.SetBool("Active", _active);
     }
 }
